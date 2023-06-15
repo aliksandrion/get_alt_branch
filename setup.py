4177010f4bc28f4b5370f08e006742cc9fb959a0
aliksandrion <aliksandrion@gmail.com>
2023-06-15 14:42:10 +0300

    Add library to PyPi

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name="get_alt_branches",
-    version="0.0.1",
+    name="getaltbranches",
+    version="0.1",
     author="Alexander Seropyan",
     author_email="aliksandrion@gmail.com",
     description="A lightweight library for getting branches from public REST API",
-    long_description="README.md",
+    long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/aliksandrion/get_alt_branches",
+    url="https://github.com/aliksandrion/get_alt_branch",
     packages=find_packages(),
-    install_requires='requirements_dev.txt',
+    install_requires=["requests"],
     classifiers=[
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
